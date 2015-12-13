277403973c61c519992873e73678296eb9ace96a
mateomtb <mateomtb@gmail.com>
2015-12-13 16:09:26 -0700

    second plot created small change to plot1 to turn off device

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-plot1 <- function (reload) {
+plot2 <- function (reload) {
   library(sqldf)
   if (reload == TRUE) {
     selector <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
@@ -9,12 +9,13 @@ plot1 <- function (reload) {
   
   #system.time(house <- readRDS('household_power_consumption.RDS'))
   house <- readRDS('household_power_consumption.RDS')
-  
+  house$datetime <- as.POSIXct(strptime(paste(house$Date,house$Time), "%d/%m/%Y %H:%M:%S"))
+  head(house)
   # open device
-  png(filename='plot1.png',width=480,height=480,units='px')
+  png(filename='plot2.png',width=480,height=480,units='px')
   
   # plot data
-  hist(house$Global_active_power,main='Global Active Power',xlab='Global Active Power (kilowatts)',col='red')
+  plot(house$datetime,house$Global_active_power,ylab='Global Active Power (kilowatts)', xlab="", type="l")
   
   # Turn off device
   dev.off()
